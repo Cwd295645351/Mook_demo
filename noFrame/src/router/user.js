5fadead9fbf8f181a9911f91cc9b21ce9e9d0677
Cwd295645351 <295645351@qq.com>
2020-06-14 11:03:26 +0800

    设置session

--- noFrame/src/router/user.js
+++ noFrame/src/router/user.js
@@ -1,20 +1,49 @@
-const { loginCheck } = require("../constroller/user");
-const { SuccessModel, ErrorModel } = require("../model/resMdodel");
+const {login} = require("../constroller/user");
+const {SuccessModel,ErrorModel} = require("../model/resMdodel");
+
+// 设置cookie过期时间
+const getCookieExpires = () => {
+  const d = new Date();
+  d.setTime(d.getTime() + 24 * 60 * 60 * 1000);
+  console.log("d.toGMTString() is ", d.toGMTString());
+  return d.toGMTString();
+};
 
 const handleUserRouter = (req, res) => {
   const method = req.method; // GET POST
 
   // 登录
-  if (method === "POST" && req.path === "/api/user/login") {
-    const { username, password } = req.body;
-    const result = loginCheck(username, password);
-    return result.then(data=>{
-      if(data.username){
+  if (method === "GET" && req.path === "/api/user/login") {
+    // const { username, password } = req.body;
+    const {
+      username,
+      password
+    } = req.query;
+    const result = login(username, password);
+    return result.then((data) => {
+      if (data.username) {
+        // 设置session
+        req.session.username = data.username;
+        req.session.realname = data.realname;
+
+        console.log("req.session is ", req.session)
+
         return new SuccessModel();
       }
-      return new ErrorModel("登录失败")
-    })
-    
+      return new ErrorModel("登录失败");
+    });
+  }
+
+  // 登录验证的测试
+  if (method === "GET" && req.path === "/api/user/login-test") {
+    if (req.session.username) {
+      return Promise.resolve(
+        new SuccessModel({
+          session: req.session,
+        })
+      );
+    }
+    return Promise.resolve(new ErrorModel("尚未登录"));
   }
 };
 
