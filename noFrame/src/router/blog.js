bd47be5b7cc684b6bae612bf83b524943d0d75b3
Cwd295645351 <295645351@qq.com>
2020-06-15 00:12:15 +0800

    添加统一的登录验证

--- noFrame/src/router/blog.js
+++ noFrame/src/router/blog.js
@@ -8,7 +8,17 @@ const {
   updateBlog,
   delBlog,
 } = require("../constroller/blog");
-const { SuccessModel, ErrorModel } = require("../model/resMdodel");
+const {
+  SuccessModel,
+  ErrorModel
+} = require("../model/resMdodel");
+
+// 统一的登录验证函数
+const loginCheck = (req) => {
+  if (!req.session.username) {
+    return Promise.resolve(new ErrorModel("尚未登录"));
+  }
+}
 
 const handleBlogRouter = (req, res) => {
   const method = req.method;
@@ -42,16 +52,28 @@ const handleBlogRouter = (req, res) => {
   if (method === "POST" && req.path === "/api/blog/new") {
     // const data = newBlog(req.body);
     // return new SuccessModel(data);
-    const author = "zhangsan";
-    req.body.author = author; // 假数据，待开发登录时再改成真实数据
+
+    const loginCheckResult = loginCheck(req);
+    if (loginCheckResult) {
+      //未登录
+      return loginCheckResult
+    }
+
+    req.body.author = req.session.username; 
     const result = newBlog(req.body);
-    return result.then((data) => {
+    return result.then((data) => { 
       return new SuccessModel(data);
     });
   }
 
   // 更新一篇博客
   if (method === "POST" && req.path === "/api/blog/update") {
+    const loginCheckResult = loginCheck(req);
+    if (loginCheckResult) {
+      //未登录
+      return loginCheckResult
+    }
+
     const result = updateBlog(id, req.body);
     return result.then((val) => {
       if (val) {
@@ -64,8 +86,13 @@ const handleBlogRouter = (req, res) => {
 
   // 删除一篇博客
   if (method === "POST" && req.path === "/api/blog/del") {
-    const author = "zhangsan";
-    req.body.author = author; // 假数据，待开发登录时再改成真实数据
+    const loginCheckResult = loginCheck(req);
+    if (loginCheckResult) {
+      //未登录
+      return loginCheckResult
+    }
+
+    req.body.author = req.session.username; 
     const result = delBlog(id, author);
     return result.then((val) => {
       if (val) {
