95bbdfc6faab5ade45a4f1f5521a6bbfaf58cf44
Cwd295645351 <295645351@qq.com>
2020-06-14 11:01:53 +0800

    设置cookie及其过期时间并返回给客户端

--- noFrame/app.js
+++ noFrame/app.js
@@ -5,6 +5,17 @@ const querystring = require("querystring");
 const handleBlogRouter = require("./src/router/blog");
 const handleUserRouter = require("./src/router/user");
 
+// 设置cookie过期时间
+const getCookieExpires = () => {
+  const d = new Date();
+  d.setTime(d.getTime() + 24 * 60 * 60 * 1000);
+  console.log("d.toGMTString() is ", d.toGMTString());
+  return d.toGMTString();
+};
+
+// session 数据
+const SESSION_DATA = {};
+
 //用于处理 post data
 const getpostData = (req) => {
   const promise = new Promise((resolve, reject) => {
@@ -43,14 +54,47 @@ const serverHandle = (req, res) => {
   // 解析query
   req.query = querystring.parse(url.split("?")[1]);
 
+  // 解析cookie
+  req.cookie = {};
+  const cookieStr = req.headers.cookie || ""; // k1=v1;k2=v2;k3=v3
+  cookieStr.split(";").forEach((item) => {
+    if (!item) {
+      return;
+    }
+    const arr = item.split("=");
+    const key = arr[0].trim();
+    const val = arr[1].trim();
+    req.cookie[key] = val;
+  });
+
+  // 解析session
+  let needSetCookie = false;
+  let userId = req.cookie.userid;
+  if (userId) {
+    if (!SESSION_DATA[userId]) {
+      SESSION_DATA[userId] = {};
+    }
+  } else {
+    needSetCookie = true;
+    userId = `${Date.now()}_${Math.random()}`;
+    SESSION_DATA[userId] = {};
+  }
+  req.session = SESSION_DATA[userId];
+
   //处理post data
   getpostData(req).then((postData) => {
     req.body = postData;
 
-    // 处理 blog 路由
+    // 处理 blog 路由  
     const blogResult = handleBlogRouter(req, res);
     if (blogResult) {
       blogResult.then((blogData) => {
+        if (needSetCookie) {
+          res.setHeader(
+            "Set-Cookie",
+            `userid=${userId};path=/; httpOnly; expires=${getCookieExpires()}`
+          );
+        }
         res.end(JSON.stringify(blogData));
       });
       return;
@@ -65,13 +109,21 @@ const serverHandle = (req, res) => {
     const userResult = handleUserRouter(req, res);
     if (userResult) {
       userResult.then((userData) => {
+        if (needSetCookie) {
+          res.setHeader(
+            "Set-Cookie",
+            `userid=${userId};path=/; httpOnly; expires=${getCookieExpires()}`
+          );
+        }
         res.end(JSON.stringify(userData));
       });
       return;
     }
 
     // 未命中路由,返回 404
-    res.writeHead(404, { "Content-type": "text/plain" });
+    res.writeHead(404, {
+      "Content-type": "text/plain"
+    });
     res.write("404 Not Found\n");
     res.end();
   });
