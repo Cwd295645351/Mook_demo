836c859c4b44a1677e11fbad6a8db2347d4208a9
Cwd295645351 <295645351@qq.com>
2020-07-16 00:03:09 +0800

    防止sql注入和xss攻击

--- noFrame/src/constroller/blog.js
+++ noFrame/src/constroller/blog.js
@@ -1,12 +1,17 @@
 /*
   controller处理数据，只关心数据并返回数据
  */
-const { exec } = require("../db/mysql");
+const xss=require("xss")
+const {
+  exec,
+  escape
+} = require("../db/mysql");
 
 const getList = (author, keyword) => {
+  author = escape(author);
   let sql = `select * from blogs where 1=1 `;
   if (author) {
-    sql += `and author='${author}' `;
+    sql += `and author=${author} `;
   }
   if (keyword) {
     sql += `and title like '%${keyword}%' `;
@@ -20,7 +25,8 @@ const getList = (author, keyword) => {
 };
 
 const getDetail = (id) => {
-  const sql = `select * from blogs where id='${id}';`;
+  id = escape(id);
+  const sql = `select * from blogs where id=${id};`;
   return exec(sql).then((rows) => {
     return rows[0];
   });
@@ -29,14 +35,19 @@ const getDetail = (id) => {
 const newBlog = (blogData = {}) => {
   //blogData 是一个博客对象，包含title, content, author属性
   // console.log("newBlog blogData...", blogData);
-  const title = blogData.title;
-  const content = blogData.content;
-  const author = blogData.author;
-  const createTime = Date.now();
+  let title = xss(blogData.title);
+  let content = xss(blogData.content);
+  let author = blogData.author;
+  let createTime = Date.now();
+
+  title = escape(title);
+  content = escape(content);
+  author = escape(author);
+
 
   const sql = `
     insert into blogs (title, content, createtime, author) 
-    value ('${title}', '${content}', ${createTime}, '${author}')`;
+    value (${title}, ${content}, ${createTime}, ${author})`;
   return exec(sql).then((insertData) => {
     console.log("insertData is ", insertData);
     return {
@@ -50,11 +61,14 @@ const updateBlog = (id, blogData = {}) => {
   //blogData 是一个博客对象，包含title content属性
   // console.log("update blog", id, blogData);
 
-  const title = blogData.title;
-  const content = blogData.content;
+  let title = xss(blogData.title);
+  let content = xss(blogData.content);
+
+  title = escape(title);
+  content = escape(content);
 
   const sql = `
-    update blogs set title='${title}', content='${content}' where id=${id}
+    update blogs set title=${title}, content=${content} where id=${id}
   `;
   return exec(sql).then((updateData) => {
     console.log("updateData is ", updateData);
@@ -67,7 +81,11 @@ const updateBlog = (id, blogData = {}) => {
 
 const delBlog = (id, author) => {
   // id 就是要删除博客的 id
-  const sql = `delete from blogs where id='${id}' and author='${author}'`;
+
+  id = escape(id);
+  author = escape(author);
+
+  const sql = `delete from blogs where id=${id} and author=${author}`;
   return exec(sql).then((delData) => {
     console.log("delData is ", delData);
     if (delData.affectedRows > 0) {
