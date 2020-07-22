a09c8a241ad5742bfded929baa7557a71c8457a5
Cwd295645351 <295645351@qq.com>
2020-07-22 23:43:16 +0800

    添加session和redis中间件

--- Express/blog-express/app.js
+++ Express/blog-express/app.js
@@ -1,13 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const session = require("express-session");
+const RedisStore = require("connect-redis")(session);
 
 /* var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users'); */
-const blogRouter = require('./routes/blog');
-const userRouter = require('./routes/user');
+const blogRouter = require("./routes/blog");
+const userRouter = require("./routes/user");
 
 var app = express();
 
@@ -15,19 +17,36 @@ var app = express();
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: false
-}));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
+const redisClient = require("./db/redis");
+const sessionStore = new RedisStore({
+  client: redisClient
+});
+app.use(
+  session({
+    secret: "SFsfa#156_156",
+    cookie: {
+      path: "/", // 默认配置
+      httpOnly: true, //默认配置
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+    store: sessionStore
+  })
+);
+
 /* app.use('/', indexRouter);
 app.use('/users', usersRouter); */
-app.use('/api/blog', blogRouter);
-app.use('/api/user', userRouter);
-
+app.use("/api/blog", blogRouter);
+app.use("/api/user", userRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -38,11 +57,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'dev' ? err : {};
+  res.locals.error = req.app.get("env") === "dev" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
